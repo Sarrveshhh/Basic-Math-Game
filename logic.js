6ecfa8d5fb2841bda65c56ff03b60d15010b9d7c
sarrveshhh <sarrveshm02@gmail.com>
2022-11-19 17:14:33 +0530

    fixed some bugs

--- logic.js
+++ logic.js
@@ -1,36 +1,31 @@
+//initializing
 let n1Id=document.getElementById("num1");
 let n2Id=document.getElementById("num2");
 let sId=document.getElementById("sign");
-let subId=document.getElementById("sub");
+let nextId=document.getElementById("next");
+let subId = document.getElementById("submit");
 let inpId=document.getElementById("inp");
-let rId=document.getElementById("r");
-const val = parseInt(document.querySelector('input').value);
-var n1 = parseInt(n1Id);
-var n2 = parseInt(n2Id);
-var n3 = parseInt(val);
-var n4 = n1 + n2;
-subId.addEventListener("click",function(){
-n1Id.textContent=Math.floor(Math.random()*100);
-n2Id.textContent=Math.floor(Math.random()*100);
-let h = ["+", "-", "*","/"];
-let rndWord = h[Math.floor(Math.random()*h.length)];
-sId.innerHTML = rndWord;
-let right=0;
-let wrong=0;
-if(rndWord===h[0])
-{
-      rId.innerText="addition";
-}
-if(rndWord===h[1])
-{
-    rId.innerText="subtraction";
-}
-if(rndWord===h[2])
-{
-    rId.innerText="multiplication";
-}
-if(rndWord===h[3])
-{
-    rId.innerText="division";
-}
+let answerId=document.getElementById("answer");
+
+var score = 0;
+
+let h = ["+", "-", "*", "/"];
+
+nextId.addEventListener("click",function(){
+
+    n1Id.textContent=Math.floor(Math.random()*50);
+    n2Id.textContent=Math.floor(Math.random()*50);
+    let operator = h[Math.floor(Math.random()*h.length)];
+    sId.innerText = operator;
+});
+
+subId.addEventListener("click", function(){
+    let actualAnswer = parseInt(n1Id.textContent + n2Id.textContent);
+    let inputAnswer = parseInt(inpId.value);
+    
+    if(actualAnswer === inputAnswer){
+        score++;
+    }
+    answerId.innerText = score;
+    
 });
